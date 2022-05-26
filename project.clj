49dcd95910e2bf1788c412e5e8dfc2892c229b52
Achint Sandhu <sandhu@7theta.com>
2022-05-26 14:54:54 -0400

    Upversion shadow-cljs

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/structor "0.8.2"
+(defproject com.7theta/structor "0.8.3"
   :description "shadow-cljs and tailwind builds"
   :url "https://github.com/7theta/structor"
   :license {:name "MIT License"
@@ -15,7 +15,7 @@
   :dependencies [[com.7theta/crusta "1.0.0"]
                  [com.7theta/tailwind "0.4.0"]
                  [binaryage/devtools "1.0.6"]
-                 [thheller/shadow-cljs "2.18.0"]
+                 [thheller/shadow-cljs "2.19.0"]
                  [metosin/jsonista "0.3.5"]
                  [integrant "0.8.0"]
                  [integrant/repl "0.3.2"]]
